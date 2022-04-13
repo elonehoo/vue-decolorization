4a95590b6a510873dd69a97272867e2eda6d1b9e
xiaoxunyao <huchengyea@163.com>
2022-04-13 19:02:52 +0800

    fix: update `github link`

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,7 +13,7 @@ import { isDark, toggleDark } from '~/composables'
       class="icon-btn"
       i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/xiaoxunyao/viden"
+      href="https://github.com/elonehoo/vue-decolorization"
       target="_blank"
       title="GitHub"
     />
